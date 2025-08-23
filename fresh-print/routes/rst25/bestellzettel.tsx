bde2684b9e3fe09c7d6c2d33244331be9696d455
Level8Broccoli <7040739+Level8Broccoli@users.noreply.github.com>
2025-08-23 08:07:29 +0200

    RST: Update Bestellzettel

--- fresh-print/routes/rst25/bestellzettel.tsx
+++ fresh-print/routes/rst25/bestellzettel.tsx
@@ -19,27 +19,32 @@ export const handler: Handlers<Data> = {
 function Part(props: { n: number }): JSX.Element {
   const number = props.n.toFixed().padStart(3, "0");
   return (
-    <div style="display: grid; grid-template-columns: 2fr 1fr; grid-template-rows: 100px 1fr; padding: 10px 20px;">
+    <div style="display: grid; grid-template-columns: 2fr 1fr; grid-template-rows: 100px 1fr; padding: 10px 20px; margin-block-end: -20px;">
       <h3>{number}</h3>
       <h3 style="border-left: 1px solid black; text-align: right;">
         {number}
       </h3>
-      <div>
-        <p class="checkbox reset">Pilzrisotto</p>
-        <p class="checkbox reset">Penne All'Arrabbiata</p>
-        <p class="checkbox reset">Penne Pesto</p>
+      <div style="position: relative; inset-block-start: -40px;">
+        <p class="checkbox reset big">Pad Thai</p>
+        <p class="checkbox reset big">Risotto</p>
+        <p class="checkbox reset big">Pilzrisotto</p>
+        <hr />
+        <div style="display: flex; gap: 1rem;">
+          <p class="checkbox reset">Mittag</p>
+          <p class="checkbox reset">Abend</p>
+        </div>
       </div>
     </div>
   );
 }
 
-function OnePage(props: { offset: number }): JSX.Element {
-  const range = [...Array(10).keys()].map((i) => i + props.offset);
+function OnePage(props: { offset: number; perPage: number }): JSX.Element {
+  const range = [...Array(props.perPage).keys()].map((i) => i + props.offset);
   return (
     <Page logo={false}>
       <div
         class="every-row every-column"
-        style="padding: 15px; display: grid; grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(5, 1fr);"
+        style="padding: 15px; display: grid; grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(5, 1fr); gap: 0;"
       >
         {range.map((n) => <Part key={n} n={n} />)}
       </div>
@@ -49,7 +54,7 @@ function OnePage(props: { offset: number }): JSX.Element {
 
 export default function Home({ data }: PageProps<Data>): JSX.Element {
   const untilNumber = 200;
-  const entriesPerPage = 10;
+  const entriesPerPage = 8;
   const numOfPages = Math.ceil(untilNumber / entriesPerPage);
   const pagesRange = [...Array(numOfPages).keys()];
   return (
@@ -59,7 +64,10 @@ export default function Home({ data }: PageProps<Data>): JSX.Element {
       </Head>
       <Pages orientation="portrait">
         {pagesRange.map((page) => (
-          <OnePage offset={data.offset + (entriesPerPage * page)} />
+          <OnePage
+            offset={data.offset + (entriesPerPage * page)}
+            perPage={entriesPerPage}
+          />
         ))}
       </Pages>
     </>
